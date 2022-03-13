ed01a98ffc31249bd20e15d36aff0c1b0c70dea4
Atomie CHEN <atomic_cwh@163.com>
2022-03-13 18:31:40 +0800

    Major: listen to system broadcast, volume keys, and change screen brightness

--- app/src/main/java/com/atomie/configlogger/MainActivity.java
+++ app/src/main/java/com/atomie/configlogger/MainActivity.java
@@ -2,13 +2,113 @@ package com.atomie.configlogger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.atomie.configlogger.MESSAGE";
+    MyBroadcastReceiver myBroadcastReceiver = new MyBroadcastReceiver();
+    SeekBar lightBar;
+    int brightness;
+    TextView textView;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lightBar = findViewById(R.id.seekBar);
+        textView = findViewById(R.id.textView);
+
+        context = getApplicationContext();
+
+        brightness = Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 0);
+        int progress = (int) (float)brightness*100/256;
+        lightBar.setProgress(progress);
+        textView.setText("进度值：" + progress + "  / 100 \n亮度值：" + brightness);
+
+        // Listen to SeekBar changes
+        lightBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                brightness = (int) (float)progress*256/100;
+                textView.setText("进度值：" + progress + "  / 100 \n亮度值：" + brightness);
+                if (Settings.System.canWrite(context)) {
+                    Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, brightness);
+                } else {
+//                    Toast.makeText(context, "Cannot write to system settings", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                if (!Settings.System.canWrite(context)) {
+                    Toast.makeText(context, "Cannot write to system settings", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                    intent.setData(Uri.parse("package:" + context.getPackageName()));
+                    startActivity(intent);
+                }
+                if (Settings.System.canWrite(context) && Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, 0) != Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL) {
+                    Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+                }
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
+    }
+
+    // Listen to the volume keys
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                // TODO
+                Toast.makeText(context, "Volume down", Toast.LENGTH_SHORT).show();
+                break;
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                // TODO
+                Toast.makeText(context, "Volume up", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        filter.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
+        filter.addAction(Intent.ACTION_MEDIA_BUTTON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SET_WALLPAPER);
+        registerReceiver(myBroadcastReceiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(myBroadcastReceiver);
+    }
+
+    public void sendMessage(View view) {
+        // Do something
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
