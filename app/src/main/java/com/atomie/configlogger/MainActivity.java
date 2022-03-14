bf60cdb8b9c21075468fc1821dcb639582c433c4
Atomie CHEN <atomic_cwh@163.com>
2022-03-14 17:16:26 +0800

    Major: add ContentObserver Minor: refactor code

--- app/src/main/java/com/atomie/configlogger/MainActivity.java
+++ app/src/main/java/com/atomie/configlogger/MainActivity.java
@@ -2,12 +2,16 @@ package com.atomie.configlogger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
@@ -18,11 +22,47 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.atomie.configlogger.MESSAGE";
-    MyBroadcastReceiver myBroadcastReceiver = new MyBroadcastReceiver();
     SeekBar lightBar;
     int brightness;
     TextView textView;
     Context context;
+
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Toast.makeText(context, action, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    // ref: https://stackoverflow.com/a/67355428/11854304
+    ContentObserver contentObserver = new ContentObserver(new Handler()) {
+        @Override
+        public void onChange(boolean selfChange) {
+            onChange(selfChange, null);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri == null) {
+                Toast.makeText(context, "onChange: uri == null", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (uri.equals(Settings.System.getUriFor(Settings.System.SCREEN_BRIGHTNESS))) {
+                int v = Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 0);
+                Log.i("MainActivity", "Brightness value: "+ v);
+                Toast.makeText(context, "Brightness value: "+ v, Toast.LENGTH_SHORT).show();
+            } else if (uri.equals(Settings.System.getUriFor(Settings.Global.AIRPLANE_MODE_ON))) {
+                int v = Settings.System.getInt(context.getContentResolver(), Settings.Global.AIRPLANE_MODE_ON, 0);
+                Log.i("MainActivity", "Airplane mode on: "+ v);
+                Toast.makeText(context, "Airplane mode on: "+ v, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "Unknown content change: "+ uri.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,11 +113,9 @@ public class MainActivity extends AppCompatActivity {
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         switch (keyCode) {
             case KeyEvent.KEYCODE_VOLUME_DOWN:
-                // TODO
                 Toast.makeText(context, "Volume down", Toast.LENGTH_SHORT).show();
                 break;
             case KeyEvent.KEYCODE_VOLUME_UP:
-                // TODO
                 Toast.makeText(context, "Volume up", Toast.LENGTH_SHORT).show();
                 break;
         }
@@ -87,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
         filter.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
@@ -94,13 +133,20 @@ public class MainActivity extends AppCompatActivity {
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_SCREEN_ON);
         filter.addAction(Intent.ACTION_SET_WALLPAPER);
-        registerReceiver(myBroadcastReceiver, filter);
+        registerReceiver(broadcastReceiver, filter);
+
+        getContentResolver().registerContentObserver(Settings.System.getUriFor(Settings.System.SCREEN_BRIGHTNESS),
+                false, contentObserver);
+        getContentResolver().registerContentObserver(Settings.System.getUriFor(Settings.Global.AIRPLANE_MODE_ON),
+                false, contentObserver);
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        unregisterReceiver(myBroadcastReceiver);
+        unregisterReceiver(broadcastReceiver);
+        getContentResolver().unregisterContentObserver(contentObserver);
     }
 
     public void sendMessage(View view) {
